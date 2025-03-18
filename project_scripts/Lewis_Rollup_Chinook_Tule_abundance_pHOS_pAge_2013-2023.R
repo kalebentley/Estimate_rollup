2eb951db4aa7a1b1211bd566661245a931a1a357
Bentley <Kale.Bentley@dfw.wa.gov>
2025-03-18 15:19:49 -0700

    renamed script file used to generate Lewis rollup estimates for 2013-2023

--- project_scripts/Lewis_Rollup_Chinook_Tule_abundance_pHOS_pAge_2013-2023.R
+++ project_scripts/Lewis_Rollup_Chinook_Tule_abundance_pHOS_pAge_2013-2023.R
@@ -2,66 +2,60 @@
 # Generate roll-up estimates for Lewis Basin tule Chinook (i.e., combining independent estimates from NF Lewis, EF Lewis, and Cedar Creek
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # Code developed by Kale Bentley
-# Updated: Aug 5, 2024
+# Updated: March 18, 2025
 
 #---------------------------------------------------------------------------------------------------------- -
 # Load functions and packages                                                 ----
 #---------------------------------------------------------------------------------------------------------- -  
 # Load functions
-  sapply(FUN = source, paste(getwd(), "functions", list.files("functions"), sep="/"))
+sapply(FUN = source, paste(getwd(), "functions", list.files("functions"), sep="/"))
 
 # Load packages
-  package_list<-c("here", "tidyverse", "openxlsx", "R2jags", "tidybayes", "shinystan") 
-  install_or_load_pack(package_list)
-  
-#---------------------------------------------------------------------------------------------------------- -  
-# Specify location and file name of input data
-#---------------------------------------------------------------------------------------------------------- -   
-## Location, file path, and file name for the input data files
-  loc_data_files<-c("Teams") # Specify the location (loc) of M-R data file: enter either "Local: (i.e., stored locally within this project) or "Teams"  
-  wd_data_files   <-c("T:/DFW-Team FP Lewis River M&E - General/Analysis/Lewis tule rollup/data") 
-      ## NOTE: if "loc_" is Teams, you must provide the full file path to the data folder 
-      ## NOTE: if "loc_" is Local, provide the file path starting at the folder name of where the data are stored within this project" 
-  file_name<-c("data_Lewis_tule_abundance_by_year_pop_origin_age_excluding_jacks_2023-10-23.csv") 
-      ## NOTE file must be stored as a .csv and file name must include the .csv suffix
+package_list<-c("here", "tidyverse", "openxlsx", "R2jags", "tidybayes", "shinystan", "glue") 
+install_or_load_pack(package_list)
 
 #---------------------------------------------------------------------------------------------------------- -
 # Import data                                                                                            ----
 #---------------------------------------------------------------------------------------------------------- -  
-  # Import
-    dat<-import_data(loc = loc_data_files, wd = wd_data_files, file_name=file_name) # Import data
-  
-  # Preview data set
-    dat %>% tibble  
-    colnames(dat)
-    dat |> group_by(SpawningYear, CommonPopName) |> count(WaterBody) |> pivot_wider(names_from = WaterBody, values_from = n)
-    dat |> filter(SpawningYear == min(SpawningYear)) |> group_by(WaterBody) |> count(Param, Age) |> pivot_wider(names_from = Age, values_from = n)
+# Specify location and file name of input data
+ui_datafiles_wd   <-c("T:/DFW-Team FP Lewis River M&E - General/Analysis/Lewis tule rollup/data")       ## full file path location of data file; if stored locally it would be something like: glue("{here::here()}/data files"); if stored on Teams, it would be something like: c("T:/DFW-Team FP Lewis River M&E - General/Analysis/Lewis tule rollup/data")  
+ui_datafile_name<-c("data_Lewis_tule_abundance_by_year_pop_origin_age_excluding_jacks_2013-2023.csv")  ## name of data file; NOTE: file must be stored as a .csv and file name must include the .csv suffix
+
+# Import
+dat<-import_data(wd = ui_datafiles_wd, file_name=ui_datafile_name) # Import data
+
+# Preview data set
+dat %>% tibble  
+colnames(dat)
+dat |> pivot_wider(id_cols = c(SpawningYear, WaterBody, Param), names_from = Age, values_from = mean) |> arrange(SpawningYear, WaterBody, Param) # Summary of estimates by SpawningYear, WaterBody, and parameter
+dat |> group_by(SpawningYear, CommonPopName) |> count(WaterBody) |> pivot_wider(names_from = WaterBody, values_from = n) # number of estimates by SpawningYear and WaterBody
 
 #---------------------------------------------------------------------------------------------------------- -
 # Generate rollup/combined estimates of abundance using log-normal moment matching                       ----
 #---------------------------------------------------------------------------------------------------------- -
 # Step 1: Specify which data columns/fields to summarize estimates of abundance
-  summary_cols <- c("SpawningYear", "CommonPopName", "Param") #typically, you should enter: c("SpawningYear", "CommonPopName", "Param")
+summary_cols <- c("SpawningYear", "CommonPopName", "Param") #typically, you should enter: c("SpawningYear", "CommonPopName", "Param")
 
 # Step 2: Using "add_param" argument, specify if you want to generate estimates by age (for each Param) or not (i.e., total), or both
-  filter_age = c("age", "total") #options: "age" or "total"; default value is c("age", "total") if NULL
+filter_age = c("age", "total") #options: "age" or "total"; default value is c("age", "total") if NULL
 
 # Step 3: Using "add_param" argument, specify if you want to also generate proportional estimates of origin (pHOS) and/or age (pAge)
-  add_param = c("pAge", "pHOS") #options: NULL -- enter as c() -- "pHOS", "pAge", or both c("pHOS", "pAge")
+add_param = c("pHOS") #options: NULL -- enter as c() -- "pHOS", "pAge", or both c("pHOS", "pAge")
 
 # Step 4: generate estimates of abundance and desired "add_param"(s)
-  est_rollup<-
-    generate_rollup_estimates(
-      data = dat
+est_rollup<-
+  generate_rollup_estimates(
+    data = dat
     , summary_cols = summary_cols
     , filter_age = filter_age
     , add_param = add_param
-    ) 
-  est_rollup|>print(n=Inf)
-  
+  ) 
+est_rollup$abund|>print(n=Inf)
+est_rollup$prop |>print(n=Inf)
+
 # Step 5 (optional): Check to make sure the mean values for NOSA plus HOSA for a given SpawningYear and CommonPopName equal TSA
-  QAQC_HOSA_NOSA_TSA(data = est_rollup)
-  
+QAQC_HOSA_NOSA_TSA(data = est_rollup$abund)
+
 #------------------------------------------------------------------------------------------------------------------- -  
 # **OPTIONAL** - Format "est_rollup" to match SPi formatting and generate an Excel file of the formatted dataset  ----
 #------------------------------------------------------------------------------------------------------------------- -   
@@ -84,7 +78,7 @@
         
 # Step 2: format est_rollup and create SPi_format
     SPi_format<-  
-      est_rollup |> 
+      est_rollup$abund |> 
       mutate(
         PopFit = PopFit_rollup,
         PopFitNotes = PopFit_Notes_rollup,
